9dbb77dc5ff1400423868dadadab8795e1b1d379
lucasbig6 <lucs.pbl290@gmail.com>
2026-02-18 21:15:59 -0300

    ajuste nas imagens

--- data/projects.js
+++ data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
     description: "Dashboard para apoio à tomada de decisão no combate à Dengue e Chikungunya na SESAPI.",
     technologies: ["Power BI"],
     liveUrl: "https://app.powerbi.com/view?r=eyJrIjoiYzAzYjAzMGMtNWVlMS00MWUxLTgwZWQtZDk1MTMyNGQyZTllIiwidCI6IjQwN2VmYjUxLTM3YTEtNDI5Mi04NWI0LTc1NTc2MjY0YjU5YiJ9",
-    image: "../assets/paineis/painel_dengue.png",
+    image: "./assets/paineis/painel_dengue.png",
     metodo: "O Processo de Atualização e construção dos dados, consiste em Ingestão dos dados do SINAN e do IBGE, criação de indicadores e dashboards interativos"
   },
   {
@@ -16,7 +16,7 @@ export const projects = [
     description: "Dashboard interativo para análise de retrospectiva musical.",
     technologies: ["Python", "Streamlit"],
     liveUrl: "https://spotifydashboard-bubzqn3groy9hhxrfvdadh.streamlit.app/",
-    image: "../assets/paineis/spotify.png",
+    image: "./assets/paineis/spotify.png",
     metodo: "importação de dados do Spotify, criação de indicadores e dashboards interativos"
   },
   {
@@ -26,7 +26,7 @@ export const projects = [
     description:"Painel focado em monitoramento de andamento de processos.",
     technologies:["Power BI"],
     liveUrl:"https://app.powerbi.com/view?r=eyJrIjoiYjZiM2NhNTQtNGU1Mi00MjhkLTkwMGEtOWUxMzFkZDRjNjBjIiwidCI6ImNhNTczMDIzLTViMWEtNDBmMS04YzNkLTJjNWVmNThlNGIzMCJ9",
-    image:"../assets/paineis/andamento.png",
+    image:"./assets/paineis/andamento.png",
     metodo:"Os dados são importados do sistema de processos SEI da SESAPI via webscraping e processados em Power BI. O painel é atualizado automaticamente a cada 24 horas."
   },
   {
@@ -46,7 +46,7 @@ export const projects = [
     description: "Painel focado em monitoramento de Atendimentos e Indicadores do Programa Saúde Digital.",
     technologies: ["Power BI"],
     liveUrl: "https://app.powerbi.com/view?r=eyJrIjoiMTIxNmFkMDAtM2VkYS00NjJiLWI1ZDctM2JjN2E2MmRiODhkIiwidCI6IjQwN2VmYjUxLTM3YTEtNDI5Mi04NWI0LTc1NTc2MjY0YjU5YiJ9",
-    image: "../assets/paineis/saude.png",
+    image: "./assets/paineis/saude.png",
     metodo: "Esse Painel consiste em Levantar os dados do Programa como um todo, focado em levantar os dados como atendimentos, Especialidades e Regiões com maior acesso a atendimentos"
 
   },
@@ -57,7 +57,7 @@ export const projects = [
     description: "Painel focado em monitoramento de lesões autoprovocadas.",
     technologies: ["Power BI", "SQL"],
     liveUrl: "https://app.powerbi.com/view?r=eyJrIjoiYmFmMDliODktN2FmZC00MWM5LTk4NTktN2ZmOWM3NzVkMTY0IiwidCI6IjQwN2VmYjUxLTM3YTEtNDI5Mi04NWI0LTc1NTc2MjY0YjU5YiJ9",
-    image: "../assets/paineis/autoprovocadas.png",
+    image: "./assets/paineis/autoprovocadas.png",
     metodo: "Painel consiste em fontes vinda do SINAM - Sistema de Informação de Agravos deNotificação e SIM - Sistema de Informação de Mortalidade"
 
   }
